2c708b5cf4499f035a5ba129417e27a170ca648a
NyanCatda <nyancatda@outlook.com>
2023-01-12 03:30:03 +0800

    完成制作

--- main.go
+++ main.go
@@ -1,13 +1,87 @@
 /*
  * @Author: NyanCatda
  * @Date: 2023-01-12 02:18:52
- * @LastEditTime: 2023-01-12 02:47:42
+ * @LastEditTime: 2023-01-12 03:26:23
  * @LastEditors: NyanCatda
  * @Description: main file
  * @FilePath: \FileListComparison\main.go
  */
 package main
 
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/nyancatda/FileListComparison/internal/ArrayComparison"
+	"github.com/nyancatda/FileListComparison/internal/File"
+	"github.com/nyancatda/FileListComparison/internal/Flag"
+)
+
 func main() {
-	
+	// 初始化参数
+	Flag.Init()
+
+	// 读取源文件夹文件列表
+	SrcFileList, err := File.GetFilesList(Flag.Flags.SrcPath)
+	if err != nil {
+		panic(err)
+	}
+	SrcFiles := make([]string, len(SrcFileList))
+	// 提取源文件夹文件列表中的文件名
+	for Index, Value := range SrcFileList {
+		SrcFiles[Index] = filepath.Base(Value)
+	}
+
+	// 读取目标文件夹文件列表
+	DestFileList, err := File.GetFilesList(Flag.Flags.DestPath)
+	if err != nil {
+		panic(err)
+	}
+	DestFiles := make([]string, len(DestFileList))
+	// 提取目标文件夹文件列表中的文件名
+	for Index, Value := range DestFileList {
+		DestFiles[Index] = filepath.Base(Value)
+	}
+
+	// 对比文件列表差异
+	New, Missing := ArrayComparison.Comparison(SrcFiles, DestFiles)
+
+	// 输出差异
+	fmt.Println("新增的文件列表：")
+	for _, Value := range New {
+		fmt.Println(Value)
+	}
+	fmt.Println("缺失的文件列表：")
+	for _, Value := range Missing {
+		fmt.Println(Value)
+	}
+
+	// 输出差异数量
+	fmt.Printf("新增了%d个文件，缺失了%d个文件\n", len(New), len(Missing))
+
+	// 判断是否需要复制文件
+	if Flag.Flags.Copy {
+		// 复制新增的文件
+		if len(New) != 0 {
+			fmt.Println("正在复制新增的文件")
+			File.MKDir("./New")
+			for _, Value := range New {
+				_, err := File.Copy(Flag.Flags.DestPath+"/"+Value, "./New/"+filepath.Base(Value))
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+		// 复制缺失的文件
+		if len(Missing) != 0 {
+			fmt.Println("正在复制缺失的文件")
+			File.MKDir("./Missing")
+			for _, Value := range Missing {
+				_, err := File.Copy(Flag.Flags.SrcPath+"/"+Value, "./Missing/"+filepath.Base(Value))
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
 }
